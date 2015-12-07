8db0f7e7572a052ff3c1cc4f5a7eedab2afeb144
naiveJeanne <412797615@qq.com>
2015-12-08 02:56:40 +0800

    再同步一部分

--- src/service/IUserService.java
+++ src/service/IUserService.java
@@ -1,10 +1,13 @@
 package service;
+
 import domain.UserAccount;
 import exception.UserExistException;
+
 //提供给用户的服务的接口
 public interface IUserService {
-	//注册服务
+	// 注册服务
 	void registerUser(UserAccount user) throws UserExistException;
-	//登录服务
-	UserAccount loginUser(String uName,String uPwd);
+
+	// 登录服务
+	UserAccount loginUser(String uName, String uPwd);
 }
