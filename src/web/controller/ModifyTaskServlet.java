2a038587303d547506c61589abb28c77d2e49332
remonly <809336646@qq.com>
2015-12-26 21:41:03 +0800

    完善其他三个Servlet中

--- src/web/controller/ModifyTaskServlet.java
+++ src/web/controller/ModifyTaskServlet.java
@@ -1,6 +1,7 @@
 package web.controller;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import domain.UserAccount;
+import exception.TaskException;
+import service.ITaskService;
 import service.IUserService;
+import service.impl.TaskServiceImpl;
 import service.impl.UserServiceImpl;
 import util.WebUtils;
 import web.formbean.CreateTaskFormBean;
@@ -30,36 +34,36 @@ public class ModifyTaskServlet extends HttpServlet{
     
     protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 
-    	int thisType = Integer.parseInt(request.getParameter("thisType"));
-    	int thatType = Integer.parseInt(request.getParameter("thatType"));
-    	
     	/* 将表单数据存储到formbean中 */
     	CreateTaskFormBean formBean = WebUtils.request2Bean(request, CreateTaskFormBean.class);
     	UserAccount user = (UserAccount)request.getSession().getAttribute("user");
     	formBean.setOwner(user.getMailAccount());
     	
-    	/* 对用户进行扣费,并更新DB中的用户信息（余额、消费记录、任务序列） */
-    	IUserService userService = new UserServiceImpl();
-    	try {
-    		userService.Charge(user.getMailAccount(),formBean.getTaskName());
-    	}
-    	catch()
-    	{
-    		
-    	}
     	
-    	/* 将formBean存储到DB中 */
+    	/* 在DB中更新相应的TaskFormBean*/
     	ITaskService taskService = new TaskServiceImpl();
+    	boolean flag = false;
     	try {
-    		taskService.storeTask(formBean);
+    		flag = taskService.modifyTask(formBean);
     	}
-    	catch()
+    	catch(TaskException e)
     	{
-    		
+    		e.printStackTrace();
     	}
     	
-    	/* 返回任务信息给用户 */
-    	
+    	/* 向前端返回修改是否成功 */
+    	if(flag){
+    		response.setHeader("Content-type","text/html;charset=UTF-8");//向浏览器发送一个响应头，设置浏览器的解码方式为UTF-8
+		    String data = "任务已修改。";
+		    OutputStream stream = response.getOutputStream();
+		    stream.write(data.getBytes("UTF-8")); 
+    	}
+    	else {
+    		response.setHeader("Content-type","text/html;charset=UTF-8");//向浏览器发送一个响应头，设置浏览器的解码方式为UTF-8
+		    String data = "修改任务失败。 ";
+		    OutputStream stream = response.getOutputStream();
+		    stream.write(data.getBytes("UTF-8"));
+		}
     }
     
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/web/controller/ViewTaskServlet.java
+++ src/web/controller/ViewTaskServlet.java
@@ -1,11 +1,23 @@
 package web.controller;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Vector;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.threads.TaskQueue;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+import domain.Task;
+import domain.UserAccount;
+import exception.TaskException;
+import service.ITaskService;
+import service.impl.TaskServiceImpl;
 
 /**
  * 处理查看任务的Servlet
@@ -22,7 +34,23 @@ public class ViewTaskServlet {
     }
     
     protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+    	/* 获取当前session的用户名 */
+    	UserAccount user = (UserAccount)request.getSession().getAttribute("user");
+    	String userMailAccount = user.getMailAccount();
     	
+    	/* 从数据库中获取该用户名下的任务列表 */
+    	ITaskService taskService = new TaskServiceImpl();
+    	Vector<Task> taskList = null;
+    	try {
+    		taskList = taskService.findTaskByMailAccount(userMailAccount);
+    	}
+    	catch(TaskException e)
+    	{
+    		e.printStackTrace();
+    	}
+    	/* 将任务列表通过Session传递到前端 */
+    	request.getSession().setAttribute("list", taskList);
+    	/* 待完成 */
     }
     
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
